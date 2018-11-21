45c7c87f5f829733fbb5ac6f66ffc3d1fd662080
Janaina Dias <ninadiasx@outlook.com>
2018-11-21 13:06:32 +0000

    Login helper

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,2 +1,13 @@
 module LoginHelper
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    
+    def log_out
+        session.delete(:user_id)
+    end
+    
+    def logged_in?
+        !session[:user_id].nil?
+    end
 end
