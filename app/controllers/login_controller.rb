eeab38bad23933152f54e1452d859ae2eae41a0f
Janaina Dias <ninadiasx@outlook.com>
2018-11-21 13:09:55 +0000

    Login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,27 @@
 class LoginController < ApplicationController
+  include LoginHelper
+  
   def auth
+    @parametros = login_params
+    @user = Usuario.find_by(email: @parametros[:email])
+    if(@user && @user.authenticate(@parametros[:password]))
+      log_in @user
+      redirect_to usuario_show_path(@user) #favor mudar dps
+    else
+      redirect_to login_path
+    end
   end
 
   def login
   end
 
   def logout
+    log_out
+    redirect_to root_path
   end
+  
+    private 
+    def login_params
+      params.require(:login).permit(:email, :password) #pode ser que mude
+    end
 end
