45e6fcaa2c40eed2073525355b2a61837bb1fbcc
Janaina Dias <ninadiasx@outlook.com>
2018-11-21 13:04:13 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'login/auth'
-  get 'login/login'
-  get 'login/logout'
+  post 'auth' => "login#auth", as: "login_auth" 
+  get 'login' => "login#login", as: "login"
+  get 'logout' => "login#logout", as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
