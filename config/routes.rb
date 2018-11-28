ca35d6cd54425bb906d687faceded33707f4b671
Janaina Dias <ninadiasx@outlook.com>
2018-11-28 22:27:53 +0000

    DB Usuario atualizado

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   post 'auth' => "login#auth", as: "login_auth" 
   get 'login' => "login#login", as: "login"
   get 'logout' => "login#logout", as: "logout"
+  get 'usuario/new'
+  get 'usuario/show/:id' => "usuario#show", as: "usuario_show"
+  post 'usuario/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
