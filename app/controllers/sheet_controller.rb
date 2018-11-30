a223e7c660af5c3d5223af1c26deaf0604357eb2
Janaina Dias <ninadiasx@outlook.com>
2018-11-30 14:54:49 +0000

    Ficha trocada para Ruby

--- app/controllers/sheet_controller.rb
+++ app/controllers/sheet_controller.rb
@@ -1,4 +1,14 @@
 class SheetController < ApplicationController
   def new
+    @sheet = Sheet.new
+  end
+  
+  def dragonborn
+    @attr = [0,0,0,0,0,0]
+    @size = "Medium"
+    @speed = 30
+    @lang = ""
+    @prof = []
+    @features = []
   end
 end
