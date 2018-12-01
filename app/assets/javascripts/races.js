760127c925ccc35bc228e0ade148ddd8955b2dd5
Janaina Dias <ninadiasx@outlook.com>
2018-12-01 15:04:47 +0000

    Generalização Classes

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -2,10 +2,7 @@ function dragonborn(){
     var ability_inc = [2,0,0,0,0,1] //0 - Strenght | 1 - Dexterity | 2 - Constitution | 3 - Intelligence | 4 - Wisdom | 5 - Charisma
     var ability = ["Strenght","Dexterity","Constitution","Intelligence","Wisdom","Charisma"]
     var cont = 0
-    var size = "Medium"
-    var speed = 30
-    var lang = ["Common","Draconic"]
-    var prof = ["Battleaxe", "Handaxe", "Light Hammer", "Warhammer"]
+    var class_feature = ["Medium",30,"Common","Draconic"]
     var draconic_color = ["Black","Blue","Brass","Bronze","Copper","Gold","Green","Red","Silver","White"]
     var draconic_damage_type = ["Acid","Lightning","Fire","Lightning","Acid","Fire","Poison","Fire","Cold","Cold"]
     var form_element;
@@ -16,8 +13,21 @@ function dragonborn(){
         form_element.innerHTML = ability[cont] + ": " + ability_inc[cont]
         ul.appendChild(form_element)
         cont++}
-    
     div_feature.appendChild(ul)
+    form_element = document.createElement('label')
+    form_element.innerHTML = "Size: " + class_feature[0] + " Speed: " + class_feature[1] + " Languagues: " + class_feature[2] + class_feature[3]
+    div_feature.appendChild(form_element)
+    cont = 0
+    form_element = document.createElement('select');
+    var option;
+    while (cont < draconic_color.length){
+            option = document.createElement('option');
+            option.value = draconic_color[cont];
+            option.text = draconic_color[cont] + " - " + draconic_damage_type[cont];
+            form_element.appendChild(option)
+            cont++
+    }
+    div_feature.appendChild(form_element)
     }
     
             /*divFeature.appendChild(formelement);
