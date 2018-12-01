2af3af0933b29d83475904df1d3c5138d4132fdf
Janaina Dias <ninadiasx@outlook.com>
2018-12-01 16:41:02 +0000

    Races atualizadas JS

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,3 +1,9 @@
+function checkAttr(ability_inc){
+    var cont=0;
+    while (cont <= 5){
+        document.getElementById('attr'+cont).innerHTML = ability_inc[cont]
+    cont++}
+}
 function dragonborn(){
     var ability_inc = [2,0,0,0,0,1] //0 - Strenght | 1 - Dexterity | 2 - Constitution | 3 - Intelligence | 4 - Wisdom | 5 - Charisma
     var ability = ["Strenght","Dexterity","Constitution","Intelligence","Wisdom","Charisma"]
@@ -6,20 +12,21 @@ function dragonborn(){
     var draconic_color = ["Black","Blue","Brass","Bronze","Copper","Gold","Green","Red","Silver","White"]
     var draconic_damage_type = ["Acid","Lightning","Fire","Lightning","Acid","Fire","Poison","Fire","Cold","Cold"]
     var form_element;
-    var ul = document.createElement('ul')
-    var div_feature = document.getElementById('features')
-    while (cont <= 5){
-        form_element = document.createElement('li')
-        form_element.innerHTML = ability[cont] + ": " + ability_inc[cont]
-        ul.appendChild(form_element)
-        cont++}
-    div_feature.appendChild(ul)
+    var ul = document.createElement('ul');
+    checkAttr(ability_inc)
+    var div_feature = document.getElementById('features');
+    div_feature.appendChild(ul);
     form_element = document.createElement('label')
-    form_element.innerHTML = "Size: " + class_feature[0] + " Speed: " + class_feature[1] + " Languagues: " + class_feature[2] + class_feature[3]
+    form_element.innerHTML = "Size: " + class_feature[0] + "<br>Speed: " + class_feature[1] + "<br>Languagues: " + class_feature[2] +" "+ class_feature[3]
     div_feature.appendChild(form_element)
     cont = 0
+    form_element = document.createElement('label');
+    form_element.innerHTML = "<br>Draconic Ancestry: "
+     div_feature.appendChild(form_element)
     form_element = document.createElement('select');
-    var option;
+    var option = document.createElement('option');
+    option.text = ". . ."
+    form_element.appendChild(option)
     while (cont < draconic_color.length){
             option = document.createElement('option');
             option.value = draconic_color[cont];
