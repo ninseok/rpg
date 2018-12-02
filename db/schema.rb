03dad377e712366dc0e776af67ac31d732775396
Janaina Dias <ninadiasx@outlook.com>
2018-12-02 21:37:07 +0000

    página de index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,86 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_181520) do
+ActiveRecord::Schema.define(version: 2018_12_02_205812) do
+
+  create_table "features", force: :cascade do |t|
+    t.string "name"
+    t.integer "level"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "proficiencies", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "race_features", force: :cascade do |t|
+    t.integer "race_id"
+    t.integer "feature_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_id"], name: "index_race_features_on_feature_id"
+    t.index ["race_id"], name: "index_race_features_on_race_id"
+  end
+
+  create_table "race_languages", force: :cascade do |t|
+    t.integer "race_id"
+    t.integer "language_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language_id"], name: "index_race_languages_on_language_id"
+    t.index ["race_id"], name: "index_race_languages_on_race_id"
+  end
+
+  create_table "race_proficiencies", force: :cascade do |t|
+    t.integer "race_id"
+    t.integer "proficiency_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["proficiency_id"], name: "index_race_proficiencies_on_proficiency_id"
+    t.index ["race_id"], name: "index_race_proficiencies_on_race_id"
+  end
+
+  create_table "race_skills", force: :cascade do |t|
+    t.integer "race_id"
+    t.integer "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_race_skills_on_race_id"
+    t.index ["skill_id"], name: "index_race_skills_on_skill_id"
+  end
+
+  create_table "races", force: :cascade do |t|
+    t.string "name"
+    t.integer "str"
+    t.integer "dex"
+    t.integer "con"
+    t.integer "int"
+    t.integer "wis"
+    t.integer "cha"
+    t.string "size"
+    t.integer "speed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.string "attribute"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "usuarios", force: :cascade do |t|
     t.string "name"
