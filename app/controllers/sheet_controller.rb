aae6fb48436a0add40e5478237c51c1c7975bd45
Janaina Dias <ninadiasx@outlook.com>
2018-12-03 14:54:25 +0000

    Seeds atualizado

--- app/controllers/sheet_controller.rb
+++ app/controllers/sheet_controller.rb
@@ -1,4 +1,5 @@
 class SheetController < ApplicationController
   def new
+    @lang = Language.new
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Race.create([{name:'Dragonborn', str:2, dex:0, con:0, int:0 ,wis:0 ,cha:1, size:'Medium', speed:30},
+{name:'Forest Gnome', str:0, dex:1, con:0, int:2 ,wis:0 ,cha:0, size:'Small', speed:25},
+{name:'Half-Elf', str:0, dex:1, con:0, int:0 ,wis:0 ,cha:2, size:'Medium', speed:30},
+{name:'Half-Orc', str:2, dex:0, con:1, int:0 ,wis:0 ,cha:0, size:'Medium', speed:30},
+{name:'High-Elf', str:0, dex:2, con:0, int:1 ,wis:0 ,cha:0, size:'Medium', speed:30}])
+
+Skill.create([{name:'Acrobatics',attr:'dex'},{name:'Animal Handling',attr:'wis'}])
