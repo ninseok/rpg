f5cc8e9676c1cee3d7c7399c87590bd75915a5ab
Janaina Dias <ninadiasx@outlook.com>
2018-12-03 01:43:40 +0000

    Banco atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2018_12_02_205812) do
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
-    t.string "attribute"
+    t.string "attr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
