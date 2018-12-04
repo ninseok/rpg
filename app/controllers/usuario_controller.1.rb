3414abcb661cc45d47c4e1ad0c39096a5a8a67bd
Janaina Dias <ninadiasx@outlook.com>
2018-12-04 01:09:41 +0000

    teste do form de sheets

--- app/controllers/usuario_controller.1.rb
+++ app/controllers/usuario_controller.1.rb
@@ -1,19 +1,18 @@
 class UsuarioController < ApplicationController
   include LoginHelper
+  include SheetController
   
   def new
     @user = Usuario.new
   end
 
   def show
-     @sheets = Sheet.all
     if(logged_in?)
       @user = Usuario.find(params[:id])
     else
       redirect_to root_path
     end
   end
-  
 
   def create
     @user = Usuario.new(user_params)
